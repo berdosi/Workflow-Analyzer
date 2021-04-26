2e7ccd85be8bd7419bc76148e415c1b6ae326bd0
Balint Erdosi <balint@berdosi.eu>
2021-04-26 17:34:43 +0200

    make the linter happpy
    Signed-off-by: Balint Erdosi <balint@berdosi.eu>

--- analyzer/review/review.py
+++ analyzer/review/review.py
@@ -6,9 +6,15 @@ If it is not available, fall back to validationRules.json in the current module'
 from analyzer.analyze.project import Project
 
 class Review():
-    """Perform review. Generate XML output into deliverables, and verbose output on the 
+    """Perform review. Generate XML output into deliverables, and verbose output on the
     terminal. In case the review is failed, error is raised."""
     VALIDATION_FILE_NAME = "validationRules.json"
 
     def __init__(self, project: Project):
         pass
+
+    def write_deliverable(self) -> None:
+        """Write the results as XML files to be picked up  by CI/CD tools"""
+
+    def write_terminal_output(self) -> None:
+        """Write the results to the terminal."""
