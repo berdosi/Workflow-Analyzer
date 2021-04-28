8af3537b54ce26d53cd3cde2dd4524dac5b09bc6
Bálint Erdősi <balint@berdosi.eu>
2021-04-28 22:24:17 +0200

    improve documentation look on mobile

--- analyzer/render/documentation.py
+++ analyzer/render/documentation.py
@@ -20,6 +20,7 @@ OUTPUT_TEMPLATE = """<!doctype html>
 <html>
     <head>
         <meta charset='utf-8'>
+        <meta name="viewport" content="width=device-width">
         <title>{0} - Documentation</title>
         <style>
             .annotation {{
