c0d4c7737f128e8583fb644581a54c08aa3e8a46
Balint Erdosi <balint@berdosi.eu>
2021-05-03 04:16:38 +0200

    add ToC to documentation
    Signed-off-by: Balint Erdosi <balint@berdosi.eu>

--- analyzer/render/documentation.py
+++ analyzer/render/documentation.py
@@ -8,7 +8,8 @@ Create a folder deliverables/documentation, renders the contents in HTML.
 import logging
 import os
 from html import escape as e
-from typing import Iterable
+from itertools import tee
+from typing import Iterable, Optional
 
 from analyzer.analyze.project import Project
 from analyzer.analyze.workflow import Workflow, WorkflowArgument
@@ -33,6 +34,10 @@ OUTPUT_TEMPLATE = """<!doctype html>
             <h1>{0}</h1>
             <p>Version: {1}</p>
             <p class='description'>{2}</p>
+            <nav>
+                <dl>{5}
+                </dl>
+            </nav>
         </header>
         <main>
             {3}
@@ -41,8 +46,11 @@ OUTPUT_TEMPLATE = """<!doctype html>
     </body>
 </html>
 """
-
-OUTPUT_TEMPLATE_WF = """<h2>{0}</h2>
+OUTPUT_TEMPLATE_TOC_ITEM = """
+                    <dt><a href="#{0}">{1}</a></dt>
+                    <dd>{2}</dd>
+"""
+OUTPUT_TEMPLATE_WF = """<h2 id="{4}">{0}</h2>
 <p>File name: {1}</p>
 <p class='annotation'>{2}</p>
 <h3>Arguments</h3>
@@ -92,19 +100,51 @@ class Documentation():
             e(arg.annotation if arg.annotation is not None else ""))
         return str.join('', map(render_argument, arguments))
 
+    def get_workflow_relative_path(self, workflow: Workflow) -> str:
+        """Get the path of the workflow file relative to project.json."""
+        if workflow is not None:
+            return workflow.file_path[len(self.project.project_directory):]
+        return ""
+
+    def get_workflow_id(self, workflow: Workflow) -> str:
+        """Get an ID that can be used for links in the table of contents."""
+        relative_path = self.get_workflow_relative_path(workflow)
+        return relative_path.replace('-','--').replace('/','-').replace('.','_')
+
     def render_workflow_documentation(self, workflow: Workflow) -> str:
         """Generate a document fragment containing documentation for a single workflow."""
         if workflow is not None:
             annotation = workflow.get_annotation() if workflow.get_annotation() is not None else ""
             return OUTPUT_TEMPLATE_WF.format(
                 e(workflow.display_name or ""),
-                e(workflow.file_path[len(self.project.project_directory):]),
+                e(self.get_workflow_relative_path(workflow)),
                 e(annotation),
-                self.render_arguments(workflow.get_arguments()))
+                self.render_arguments(workflow.get_arguments()),
+                self.get_workflow_id(workflow))
         return ""
 
-    def get_main_workflow(self) -> Workflow:
-        """Find the full path of the main workflow from the relative path found in project.json."""
+    def render_documentation_toc(self) -> str:
+        """Render table of contents with links to sections."""
+        return self.render_documentation_toc_item(self.get_main_workflow()) + str.join(
+                    '',
+                    [ self.render_documentation_toc_item(wf)
+                        for wf in  self.__workflow_toc])
+
+    def render_documentation_toc_item(self, workflow: Workflow) -> str:
+        """Render one line of the table of contents."""
+        if workflow is None:
+            return ""
+
+        display_name = workflow.display_name
+        logging.info(workflow.file_path)
+        return OUTPUT_TEMPLATE_TOC_ITEM.format(
+            self.get_workflow_id(workflow),
+            display_name or "",
+            self.get_workflow_relative_path(workflow))
+
+    def get_main_workflow(self) -> Optional[Workflow]:
+        """Find the full path of the main workflow from the relative path found in project.json.
+        Return None in case there is none - e.g. in case of a library."""
         if self.project.main is not None:
             main_workflow_path = os.path.normpath(os.path.join(
                 self.project.project_directory, self.project.main))
@@ -115,6 +155,7 @@ class Documentation():
 
     def build_documentation(self):
         """Generate the documentation from the project."""
+        self.__workflow_toc, self.__workflow_doc = tee(self.project.workflow_files)
         with open(self.target_path, 'w') as documentation_file:
             main_workfow: Workflow = self.get_main_workflow()
             documentation_file.write(OUTPUT_TEMPLATE.format(
@@ -125,9 +166,11 @@ class Documentation():
                 str.join(
                     '',
                     [ self.render_workflow_documentation(wf)
-                        for wf in self.project.workflow_files
-                        if wf.file_path != main_workfow.file_path])
+                        for wf in self.__workflow_doc
+                        if wf.file_path != main_workfow.file_path]),
+                self.render_documentation_toc()
             ))
+        logging.info(self.render_documentation_toc())
 
     def __init__(self, project: Project):
         logging.info(project.name)
